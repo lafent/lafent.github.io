123223955e048f4e8762c5b1c3d3d90e0d77e9b9
Lee Fent <lee.fent@gmail.com>
2014-06-22 19:25:01 -0500

    First setup and blog entry.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * @author: Lee Fent <lee.fent@gmail.com>
+ * @date: 06-April-2014
+ */
+// Used to selectively highlight the "active" menu item from the list below.
+$(document).ready(function() {
+  // Activate Tablesorter on targetted tables
+  $("table.table-sortable").tablesorter();
+
+  $(document).foundation();
+});
+
